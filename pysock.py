ed700c7602e34a700e8eea19d049dddd810840fe
Some One <mirror_github@notgoogle.org>
2016-05-03 22:15:33 +0200

    refac

--- pysock.py
+++ pysock.py
@@ -1,110 +1,150 @@
 #!/usr/bin/python
 import socket
 import sys
+import random
 from urlparse import urlparse
 #https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
 
+# settings
+dbg = 1;
+protocol = None;
+domain = None;
+request_page = None;
+http_methods = ["GET", "HEAD", "CHECKOUT", "PUT", "DELETE", "POST", "LINK", "UNLINK", "CHECKIN", "TEXTSEARCH", "SPACEJUMP", "SEARCH",  "TRACE", "OPTIONS", "08HJAE98134", "                                                                            " ]
+http_options = ["HTTP/1.1","Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.0.1) Gecko/20060111 Firefox/1.5.0.1"];
+# global variables
+arg_requested_url = None;
+arg_ip = None;
 
-sysarg="";
-arg_url="";
-arg_ip="";
-arg_page="";
-page="";
-port=80;
+# flags
+methodloop_flag = 0;
 
-for ( i, sysarg) in enumerate(sys.argv):
-	if ( sysarg in ["--url", "-u"] ):
-		arg_url=sys.argv[i+1];
-        if ( sysarg in ["--host","-h"] ):
-                arg_ip = sys.argv[i+1];
-try:
-	url_split_list = arg_url.split("/");
-	domain = url_split_list[0];
-	del url_split_list[0];
-	for part in url_split_list:
-		page += ( str(part)+"/");
+# functions
+def split_url(full_uri):
+	uri_splitlist = full_uri.split("/");
+	protocol = uri_splitlist[0][:-1];
+	domain = uri_splitlist[2];
+	request_page = "";
 
-	page = page[:-1];
+	for i in range(3, len(uri_splitlist)):
+		tmp_string = "/" + uri_splitlist[i];
+		request_page += tmp_string; 
 
-except ValueError:
-	domain = arg_url;
-	page = "";
+	return protocol,domain,request_page;
 
-if ( arg_url == "" ):
-	print "Usage:";
-        print "./pysock.py --url www.desired.url --host des.ire.dho.st";
-        print "./pysock.py -u www.desired.url -h des.ire.dho.st";
-        print "./pysock.py -u www.desired.url";
+def getAddr(domain):
+	try:
+        	remote_ip = socket.gethostbyname( domain );
+	except socket.gaierror:
+        	print 'Hostname could not be resolved. Exiting'
+        	sys.exit()
 
-try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-except socket.error:
-	print 'Failed to create socket';
-	sys.exit();
+	return remote_ip;	
+
+def createSocket(protocol):
+	if ( protocol == "https" ): print "suca";
+	if ( protocol == "http" ):
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+		except socket.error:
+			print "Failed to create socket";
+		return s;
+
+
+def CallWebPage(domain, page, ip, method):
+
+	s = createSocket(protocol);
+
+	message = method  + " /" + page + " " + http_options[0] + "\r\nConnection: Keep-Alive\r\n" + http_options[1] + "\r\nHost: " + domain + "\r\n\r\n"; 
+	s.connect((ip, 80));
+
+	try:
+		s.sendall(message)
+	except socket.error:
+		print 'Send failed'
+		sys.exit()
+
+	part = None;
+	reply = "";
+	while ( part != ""):
+		part = s.recv(5575)
+		reply += part;
+
+	s.close();
+
+	headers = "";
+	body = "";
+	headers_ended = 0;
+
+
+	for line in reply.splitlines():
+		if ( headers_ended == 0 ):
+			if ( line == '' ):
+				headers_ended = 1;
+				continue;
+			line_forprint=line +"\r\n";
+			headers += line_forprint;
+		else:
+			if ( line == ''): continue;
+			else:
+				line_forprint = line + "\r\n";
+				body += line_forprint;
+	return headers,body;
 
 
 	
-try:
-	remote_ip = socket.gethostbyname( domain );
-except socket.gaierror:
-	print 'Hostname could not be resolved. Exiting'
-	sys.exit()
+
+####### MAIN ##########################
+
+# get args
+if ( len(sys.argv) > 1 ):
+	for ( i, sysarg) in enumerate(sys.argv):
+        	if ( sysarg in ["--url", "-u"] ):
+                	arg_requested_url=sys.argv[i+1];
+        	if ( sysarg in ["--host", "-h"] ):
+                	arg_ip=sys.argv[i+1];
+		if ( sysarg in ["--loopmethods", "-lp"] ):
+			methodloop_flag = 1;
+			print methodloop_flag;
+
+if ( arg_requested_url != None ):
+	protocol, domain, request_page = split_url(arg_requested_url);	
+
+if ( methodloop_flag != 1 ):
+	remote_ip = getAddr(domain);
+
+	headers,body = CallWebPage(domain, request_page, remote_ip, http_methods[0]);
+
+	id = str(random.randint(00000000,99999999));
+	bname = domain+"-"+id+".out";
+	hname = domain+"-headers-"+id+".out";
+	
+	out_file=open(bname, "w");
+	out_file.write(body);
+	out_file.close();
+	
+	out_file=open(hname, "w");
+	out_file.write(headers);
+	out_file.close();
+else:
+	for method in http_methods:
+		print method;
+		remote_ip = getAddr(domain);
+	        headers,body = CallWebPage(domain, request_page, remote_ip, method);
+
+        	id = str(random.randint(00000000,99999999));
+        	bname = domain+"-"+id+"-"+method+".out";
+        	hname = domain+"-headers-"+id+"-"+method+".out";
+
+		print "";
+		print headers;
+        	out_file=open(bname, "w");
+        	out_file.write(body);
+        	out_file.close();
+
+        	out_file=open(hname, "w");
+        	out_file.write(headers);
+        	out_file.close();
 
 
-if ( arg_ip != "" ):
-	print " Changing " + str(remote_ip)  + " in " + (arg_ip) + " for " + domain + " \n ";
-	remote_ip = arg_ip;
-
-
-http_command="GET";
-#http_command="UAT";
-http_version="HTTP/1.1";
-http_ua="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.0.1) Gecko/20060111 Firefox/1.5.0.1";
-
-message = http_command + " /" + page + " " + http_version + "\r\nConnection: Keep-Alive\r\n" + http_ua + "\r\nHost: " + domain + "\r\n\r\n"; 
-print message;
-
-s.connect((remote_ip, int(port)));
-
-try:
-	s.sendall(message)
-except socket.error:
-	print 'Send failed'
-	sys.exit()
-
-part = None;
-reply = "";
-while ( part != ""):
-	part = s.recv(5575)
-	reply += part;
-
-filename = domain + ".out";
-out_file=open(filename, "w");
-out_file.write("");
-out_file.close();
-out_file=open(filename, "a");
-
-
-headers = "";
-body = "";
-headers_ended = 0;
-
-
-for line in reply.splitlines():
-	if ( headers_ended == 0 ):
-		if ( line == '' ):
-			headers_ended = 1;
-			continue;
-		line_forprint=line +"\r\n";
-		headers += line_forprint;
-	else:
-		if ( line == ''):
-			continue;
-		else:
-			line_forprint = line + "\r\n";
-			body += line_forprint;
-		
-
-print headers;
-out_file.write(body);
-out_file.close();
+####   END ###################################
